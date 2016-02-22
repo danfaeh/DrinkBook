6df473800f383463d1243be37290ee523dd6aa95
danfaeh <danfaeh@gmail.com>
2016-02-22 14:52:03 -0800

    finished models & relationships

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,27 @@ class UsersController < ApplicationController
     render :index
   end
 
-  # def new
-  # end
+  def new
+    @user = User.new
+    render :new
+  end
 
-  # def show
-  # end
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+    login(@user) # <-- login the user
+    redirect_to "/users/#{@user.id}" # <-- go to show
+  end
 
-  # def create
-  # end
+  def show
+    @user = User.find(params[:id])
+    render = :show
+  end
 
   # def edit
   # end
+
+
 
   # def delete
   # end
